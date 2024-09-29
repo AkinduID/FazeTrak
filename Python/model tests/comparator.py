fbf9ce3f77bb0b9bd9639fbbdfab8b9402fce1f6
Akindu Induwara <76662130+AkinduID@users.noreply.github.com>
2024-09-29 19:50:45 +0530

    created a python filr to compare models

---
+++ Python/model tests/comparator.py
@@ -0,0 +1,12 @@
+import funcholistic
+import funcblazeface
+import funchaar
+import funcmtcnn
+import funcyolo
+
+# define video list
+# for each video in video list, 
+    # run the face detection functions
+    # save each image with the face detection bounding boxes and detection time
+    # plot the detection time 
+    # save the plot
